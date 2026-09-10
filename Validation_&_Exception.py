c2fad519ed61be0a94546572a12348995c1fad36
Iven Hagge <iven.hagge@bremergy.de>
2026-09-10 12:29:23 +0200

    Testeinmg CSV

--- Validation_&_Exception.py
+++ Validation_&_Exception.py
@@ -1,1 +1,14 @@
 Return = (False," only CVS is allowed")
+
+from enum import Enum
+class Category(Enum):   
+    OsTech = 1
+
+class Category(Enum):   
+    OsTech = 1
+
+class TAG(Enum) 
+    OsTech = 1
+
+class State(Enum)
+    OsTech = 1
